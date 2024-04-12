b92c688553482ef91c930f0417c79b6bab12f6cc
Nadeem Ashraf <dev.nad.ash@gmail.com>
2024-04-13 01:08:59 +0530

    Added Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react";
 import { store } from "@/store";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <>
+    <Analytics />
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </>
 );
